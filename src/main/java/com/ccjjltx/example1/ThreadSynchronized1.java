837e55e5c77ff8a376a6a965f0a560ac30e785bd
783081406 <783081406ccj@gmail.com>
2018-05-22 20:42:15 +0800

    synchronized

---
+++ src/main/java/com/ccjjltx/example1/ThreadSynchronized1.java
@@ -0,0 +1,61 @@
+package com.ccjjltx.example1;
+
+/**
+ * synchronized
+ *
+ * @author ccj
+ * @version 1.0
+ * @since 1.8
+ */
+public class ThreadSynchronized1 {
+
+    public static void main(String[] args) {
+        Outputer outputer = new ThreadSynchronized1().new Outputer();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    outputer.output1("cccc");
+                }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    outputer.output2("jjjj");
+                }
+            }
+        }).start();
+    }
+
+    class Outputer {
+        public void output1(String name) {
+            int len = name.length();
+            synchronized (this) {
+                for (int i = 0; i < len; i++) {
+                    System.out.print(name.charAt(i));
+                }
+                System.out.println();
+            }
+        }
+
+        public synchronized void output2(String name) {
+            int len = name.length();
+            for (int i = 0; i < len; i++) {
+                System.out.print(name.charAt(i));
+            }
+            System.out.println();
+        }
+    }
+}
